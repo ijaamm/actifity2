a69e2c26330ea19b84cf2c9f7dfcc00ec38b4942
ijaamm <ijammusyaf@gmail.com>
2023-03-08 16:02:10 +0700

    first commit

---
+++ coba.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){//begin
+
+    //numeric Luas, P, L
+    double Luas, P, L;  
+    //Display, "Masukan Panjangnya : "
+    cout << "Masukan Panjangnya : ";
+    //Accept p
+    cin >> p;
+    //Display, "Masukan Lebarnya : "
+    cout 
+}
