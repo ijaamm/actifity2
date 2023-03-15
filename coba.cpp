ff85013bc42ca534cb981df649b6841caec23131
ijaamm <ijammusyaf@gmail.com>
2023-03-15 12:21:01 +0700

    first commit

--- coba.cpp
+++ coba.cpp
@@ -4,11 +4,17 @@ using namespace std;
 int main (){//begin
 
     //numeric Luas, P, L
-    double Luas, P, L;  
+    int Luas, P, L;  
     //Display, "Masukan Panjangnya : "
     cout << "Masukan Panjangnya : ";
     //Accept p
-    cin >> p;
-    //Display, "Masukan Lebarnya : "
-    cout 
+    cin >> P;
+    cout << "Masukan Lebarnya :" ;
+    //Accept L
+    cin >> L;
+    //Compute Luas = P*L
+    Luas = P*L;
+    //Display, " luasnya : " + Luas
+    cout << "Luasnya : " << Luas;
+    //end
 }
